02146e85c0d1918717ead89242974e95e8b5601b
Priya1306 <108993369+Priya1306@users.noreply.github.com>
2022-07-27 20:00:26 +0530

    Add files via upload

---
+++ PrintDuplicatesInArray.java
@@ -0,0 +1,25 @@
+package week3.day2.assignments.mandatory;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class PrintDuplicatesInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {14,12,13,11,15,14,18,16,17,19,18,17,20};
+		Set<Integer> s=new TreeSet<Integer>();
+		Set<Integer> duplicate=new TreeSet<Integer>();
+		for(int i=0;i<arr.length;i++) {
+			boolean flag=s.add(arr[i]);
+
+			if(flag==false) { 
+				duplicate.add(arr[i]);
+			}
+
+		}
+		System.out.println(s);
+		System.out.println(duplicate);
+	}
+
+}
